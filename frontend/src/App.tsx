52f883b1b1d10dbe6fb722260c8b90e534176494
Prince Prabhat <princeprabhat1996@gmail.com>
2025-12-21 23:37:43 +0530

    initialized frontend with Vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { io } from "socket.io-client";
+type SData = {
+  price: number;
+  name: string;
+};
+const App = () => {
+  const [data, setData] = useState<SData[]>([]);
 
-function App() {
-  const [count, setCount] = useState(0)
+  const socket = io("http://localhost:3002");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data: { data: SData[] } = await axios.get(
+        "http://localhost:3002/api"
+      );
+      console.log("data", data);
+      setData(data.data);
+    };
+    fetchData();
+
+    socket.on("hello", (msg) => {
+      setData(msg);
+    });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="card-container">
+      {data.map((el, idx) => {
+        return (
+          <div key={idx} className="card-1">
+            <p>Stock name is: {el.name}</p>
+            <p>Stock price is: {el.price}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default App
+export default App;
